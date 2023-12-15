a129ff82f3620ce3497d792b39c3cb7487464e07
Rohithmunna31 <rohithkasna31@gmail.com>
2023-12-15 20:11:45 +0530

    added a arrow function and a object

--- firstcodeinnode.js
+++ firstcodeinnode.js
@@ -1,1 +1,20 @@
 console.log('Hello World!');
+
+let add = (a,b) => a+b;
+
+console.log(add(10,23));
+
+
+const student = {
+    name:'Rohith',
+    rollno:'1922',
+    interest:'webdevelopment',
+    greeting: ()=> {
+        console.log('this is' + this.name);
+    }
+}
+
+console.log(student);
+
+
+
