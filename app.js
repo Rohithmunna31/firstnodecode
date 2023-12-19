d94872b5230909f36704ca419d3feb146b9cb174
Rohithmunna31 <rohithkasna31@gmail.com>
2023-12-20 01:11:16 +0530

    added code only using node which takes input from user and displays it

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
-
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body>');
+  const url = req.url;
+  const method = req.method;
 
-    // Check the URL and provide different responses
-    if (req.url === '/home') {
-        res.write('<h1>Welcome home</h1>');
-    } else if (req.url === '/about') {
-        res.write('<h1>Welcome to About Us page</h1>');
-    } else if (req.url === '/node') {
-        res.write('<h1>Welcome to my Node Js project</h1>');
-    } else {
-        res.write('<h1>Page not found</h1>');
-    }
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title> This is title </title></head>");
+    res.write(
+      '<body><form action="/" method="POST"> <input type="text" name="message"> <button type="submit"> Send</button> </form></body>'
+    );
+    return;
+  }
+  if (url === "/" && method === "GET") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      res.write(`<p>  ${message} || "No input yet"</p>`);
 
-    res.write('</body>');
-    res.write('</html>');
-    res.end();
+      fs.writeFileSync("message.txt", message, () => {
+        res.statusCode = "302";
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
-
