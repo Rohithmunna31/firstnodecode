7ca589b422f06a1c4362a6d552efc62e4466a3a4
Rohithmunna31 <rohithkasna31@gmail.com>
2023-12-20 21:29:21 +0530

    cleaned up the previous code and stored clean code in new file

--- app1.js
+++ app1.js
@@ -1,52 +1,10 @@
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
 
-const filePath = path.join(__dirname, "userInput.txt");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  if (req.method === "GET") {
-    // Display the form for user input
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>User Input</h1>");
-    res.write('<form method="POST" action="/">');
-    res.write(
-      '<input type="text" name="userInput" placeholder="Enter your input" />'
-    );
-    res.write('<input type="submit" value="Submit" />');
-    res.write("</form>");
+console.log(routes.sometext);
 
-    // Read and display the previously stored input
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (!err) {
-        res.write(`<p>  ${data || "No input yet"}</p>`);
-      }
-      res.end();
-    });
-  } else if (req.method === "POST") {
-    // Handle form submission
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      const userInput = decodeURI(body).split("=")[1]; // Extract user input
-
-      // Write user input to the file
-      fs.writeFile(filePath, userInput, (err) => {
-        if (err) {
-          console.error("Error writing to file:", err);
-          res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end("Internal Server Error");
-          return;
-        }
-        res.writeHead(302, { Location: "/" }); // Redirect to GET request for the same page
-        res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes.handler);
 
 server.listen(4000, () => {
   console.log("Server running at http://localhost:4000/");
