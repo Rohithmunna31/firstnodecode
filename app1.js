f91fe05ef6988a3060af0d04364351702110555a
Rohithmunna31 <rohithkasna31@gmail.com>
2024-01-06 13:21:15 +0530

    added contact us page if client want to contact

--- app1.js
+++ app1.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const parser = require("body-parser");
 
+const path = require("path");
+
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin.js");
 
-const shopRoutes = require("./routes/shop");
+const shopRoutes = require("./routes/shop.js");
+
+const errRoutes = require("./routes/404.js");
+
+const contactRoutes = require("./routes/contact.js");
+
+const successRoutes = require("./routes/success.js");
 
 app.use(parser.urlencoded({ extended: false }));
 
-app.use('admin',adminRoutes);
-app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use("/shop", shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).send("<h1> Page not found </h1>");
-});
+app.use(errRoutes);
 
 app.listen(3000);
